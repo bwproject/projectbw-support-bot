1a283e9068c4876fd6122888688c0dfe4863a8d6
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2023-04-12 02:36:41 +0500

    Update faq.py

--- message/faq.py
+++ message/faq.py
@@ -4,7 +4,6 @@ FAQ_1='Забыли свой Пароль'
 FAQ_1_1="""\
 Если вы забыли свой пароль, используйте раздел «Забьли свой пароль» на:
 [wiki_projectbw](https://wiki.projectbw.ru/faq/#%D0%B7%D0%B0%D0%B1%D1%8B%D0%BB%D0%B8-%D0%BF%D0%B0%D1%80%D0%BE%D0%BB%D1%8C)
-<a href="http://www.example.com/">inline URL</a>
 \
 """
 FAQ_2='FAQ 2'
