3dcd3b375347032311a46e742051b927084d7205
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2023-04-12 02:02:57 +0500

    Update faq.py

--- message/faq.py
+++ message/faq.py
@@ -1,3 +1,25 @@
 #faq.py
-FAQ_1=
-FAQ_1_2=
+FAQ_1='Забыли свой Пароль'
+FAQ_1_1="""\
+Если вы забыли свой пароль, используйте раздел «Забьли свой пароль» на:
+https://wiki.projectbw.ru/faq/#%D0%B7%D0%B0%D0%B1%D1%8B%D0%BB%D0%B8-%D0%BF%D0%B0%D1%80%D0%BE%D0%BB%D1%8C
+\
+"""
+FAQ_2='FAQ 2'
+FAQ_2_1="""\
+Ответ на FAQ 2:
+https://wiki.projectbw.ru/faq/
+\
+"""
+FAQ_3='FAQ 3'
+FAQ_3_1="""\
+Ответ на FAQ 3:
+https://wiki.projectbw.ru/faq/
+\
+"""
+FAQ_ALL='Открыть все FAQ'
+FAQ_ALL_1="""\
+Все FAQ доступны в раздел «F.A.Q - Часто задаваемые вопросы» на:
+https://wiki.projectbw.ru/faq/
+\
+"""
