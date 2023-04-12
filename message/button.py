9a4c7dae0b749f94b44439a19986c3c5beb51805
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2023-04-13 00:10:03 +0500

    Update button.py

--- message/button.py
+++ message/button.py
@@ -1,8 +1,6 @@
 #button.py
 BUT_FAQ='❓F.A.Q – «часто задаваемые вопросы»'
 BUT_SUPPORT='💬 Задать вопрос'
-BUT_SUPPORT2='ProjectBW'
-BUT_SUPPORT3='Recreation'
 BUT_BACK='❌ Назад'
 BUT_MENU='✅ Главное меню'
 BUT_BOT='🔥 О Боте'
