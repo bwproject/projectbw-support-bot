47d99213a2de16b65087980cfceebfb1eaf9fd46
The_Mr_Mes109 <linkthemrmes109@gmail.com>
2023-04-12 09:40:56 +0500

    Update button.py

--- message/button.py
+++ message/button.py
@@ -1,5 +1,5 @@
 #button.py
-BUT_FAQ='F.A.Q – «часто задаваемые вопросы»'
-BUT_SUPPORT='Задать вопрос'
-BUT_BACK='Назад'
-BUT_MENU='Главное меню'
+BUT_FAQ='❓F.A.Q – «часто задаваемые вопросы»'
+BUT_SUPPORT='💬 Задать вопрос'
+BUT_BACK='❌ Назад'
+BUT_MENU='✅ Главное меню'
